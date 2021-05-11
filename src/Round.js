df6999755c3255cea7ab74b7e6c239e1426d4d19
Taylor Galloway <gallowaytaylor@outlook.com>
2021-05-11 15:02:32 -0500

    Add list of methods to test

--- src/Round.js
+++ src/Round.js
@@ -1,6 +1,7 @@
 class Round {
-  constructor() {
+  constructor(deck) {
     this.turns = 0;
+    this.deck = deck;
   }
 }
 module.exports = Round;
--- test/Round-test.js
+++ test/Round-test.js
@@ -5,11 +5,15 @@ const Turn = require('../src/Turn.js');
 const Round = require('../src/Round.js');
 
 describe('Round', () => {
+    let card1;
+    let card2;
+    let card3;
+    let deck1;
   beforeEach('Setup', () => {
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(2, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(3, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    const deck1 = new Deck([card1, card2, card3]);
+    card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    card2 = new Card(2, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    card3 = new Card(3, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    deck1 = new Deck([card1, card2, card3]);
   })
   it('should be an instance of the Round class', () => {
     const round1 = new Round();
@@ -21,5 +25,37 @@ describe('Round', () => {
     const round1 = new Round();
 
     expect(round1.turns).to.equal(0);
+  })
+
+  it('should take in a new deck and store it', () => {
+    const round1 = new Round(deck1);
+    console.log("look here", deck1);
+    console.log("card", card1);
+
+    expect(round1.deck).to.be.an.instanceOf(Deck);
+  })
+
+  it.skip('should have a returnCurrentCard method', () => {
+    const round1 = new Round();
+
+    expect(round1).to.be.an.instanceOf(Round);
+  })
+
+  it.skip('should have a takeTurn method', () => {
+    const round1 = new Round();
+
+    expect(round1).to.be.an.instanceOf(Round);
+  })
+
+  it.skip('should have a calculatePercentageCorrect method', () => {
+    const round1 = new Round();
+
+    expect(round1).to.be.an.instanceOf(Round);
+  })
+
+  it.skip('should have a endRound method', () => {
+    const round1 = new Round();
+
+    expect(round1).to.be.an.instanceOf(Round);
   })
 })
